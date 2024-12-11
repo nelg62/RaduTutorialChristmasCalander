2496a4be8557e7f703b2d5e10a02fe8917bd07f7
nelg62 <glen.harding.nz@gmail.com>
2024-12-11 15:57:15 +1300

    day 5 done

---
+++ items/bow.js
@@ -0,0 +1,32 @@
+function drawBow(ctx, x, y, size, hue) {
+  const top = y - size / 2;
+  const left = x - size / 2;
+  const right = x + size / 2;
+  const bottom = y + size / 2;
+  //   ctx.strokeRect(top, left, size, size);
+
+  ctx.beginPath();
+  ctx.fillStyle = color.normal(hue);
+  ctx.moveTo(x, y);
+  ctx.quadraticCurveTo(left, top, left, y);
+  ctx.quadraticCurveTo(left, bottom, x, y);
+  ctx.quadraticCurveTo(right, bottom, right, y);
+  ctx.quadraticCurveTo(right, top, x, y);
+  ctx.fill();
+
+  const knot = {
+    size: size * 0.25,
+    get top() {
+      return y - this.size / 2;
+    },
+    get left() {
+      return x - this.size / 2;
+    },
+    roundness: size * 0.05,
+  };
+
+  ctx.beginPath();
+  ctx.fillStyle = color.dark(hue);
+  ctx.roundRect(knot.left, knot.top, knot.size, knot.size, knot.roundness);
+  ctx.fill();
+}
