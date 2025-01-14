85c5fc0d8ed7ae3826f48c57755cc2d0103986f9
nelg62 <glen.harding.nz@gmail.com>
2025-01-14 20:29:55 +1300

    day 19 done

---
+++ items/bells.js
@@ -0,0 +1,28 @@
+function drawBells(ctx, x, y, size, hue) {
+  //   const top = y - size / 2;
+  //   const left = x - size / 2;
+  //   ctx.strokeRect(top, left, size, size);
+
+  const bell = {
+    size: size / 2,
+    y: y + size * 0.15,
+    xOffset: size * 0.2,
+    rotation: Math.PI / 6,
+  };
+
+  ctx.save();
+
+  ctx.translate(x, bell.y);
+  ctx.rotate(bell.rotation);
+  drawBell(ctx, -bell.xOffset, 0, bell.size, hue);
+  ctx.rotate(-2 * bell.rotation);
+  drawBell(ctx, +bell.xOffset, 0, bell.size, hue);
+
+  ctx.restore();
+
+  const bow = {
+    size: size * 0.7,
+    y: y - size * 0.15,
+  };
+  drawBow(ctx, x, bow.y, bow.size, color.reverse(hue));
+}
